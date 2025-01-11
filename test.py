932e85dd365d5daf3e276026da864387d52ad43b
gt <jack139@gmail.com>
2025-01-11 12:42:37 +0800

    分步检测和识别

--- test.py
+++ test.py
@@ -25,6 +25,15 @@ def ocr(img):
     result = reader.readtext(img)
     return result
 
+def detect(img):
+    horizontal_list, free_list = reader.detect(img)
+    return horizontal_list[0], free_list[0]
+
+def recognize(img, horizontal_list, free_list):
+    result = reader.recognize(img, horizontal_list, free_list)
+    return result
+
+
 def draw_boxes(img, boxes):
     h, w, c = img.shape
 
@@ -41,17 +50,33 @@ if __name__ == '__main__':
 
     img = cv2.imread(sys.argv[1])
 
-    r1 = ocr(img)
+    '''
+    # 一步 ocr
+    r3 = ocr(img)
 
-    boxes = [i[0] for i in r1]
+    boxes = [i[0] for i in r3]
+    '''
 
+    # 只检测文本
+    r1, r2 = detect(img)
+
+    print(r1)
+    print(r2)
+    
+    boxes = [[[i[0],i[2]],[i[1],i[2]],[i[1],i[3]],[i[0],i[3]]] for i in r1]
+
+    r3 = recognize(img, r1, r2)
+
+
+    print(boxes)
     draw_boxes(img, boxes)
 
-    for i in r1:
+    for i in r3:
         print(f"{i[2]:.6f}\t{i[1]}")
 
+
     json.dump(
-        r1,
+        r3,
         open('result.json', 'w', encoding='utf-8'),
         #indent=4,
         ensure_ascii=False,
